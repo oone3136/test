13edfa6096d8ae3c5b4484e61c9f162ae204f488
hilman <muhammadhilman200@gmail.com>
2024-02-07 15:23:49 +0700

    membalikan String

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("input =");
+        String input = scanner.nextLine();
+
+        String reversed = reverseString(input);
+        System.out.println("output = " + reversed);
+
+    }
+    public static String reverseString(String str) {
+
+        StringBuilder reversed = new StringBuilder();
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed.append(str.charAt(i));
+        }
+        return reversed.toString();
+    }
+}
